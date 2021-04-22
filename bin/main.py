4c0da31656f737796a635ebcf5fe9cc5a6866a99
Noah Syrkis <noah@syrkis.com>
2021-04-22 12:11:55 +0200

    first commit

---
+++ bin/main.py
@@ -0,0 +1,41 @@
+####################################################
+## Getting the data
+
+from influxdb import InfluxDBClient # install via "pip install influxdb"
+import pandas as pd
+
+client = InfluxDBClient(host='influxus.itu.dk', port=8086, username='lsda', password='icanonlyread')
+client.switch_database('orkney')
+
+def get_df(results):
+    values = results.raw["series"][0]["values"]
+    columns = results.raw["series"][0]["columns"]
+    df = pd.DataFrame(values, columns=columns).set_index("time")
+    df.index = pd.to_datetime(df.index) # Convert to datetime-index
+    return df
+
+# Get the last 90 days of power generation data
+generation = client.query(
+    "SELECT * FROM Generation where time > now()-90d"
+    ) # Query written in InfluxQL
+
+# Get the last 90 days of weather forecasts with the shortest lead time
+wind  = client.query(
+    "SELECT * FROM MetForecasts where time > now()-90d and time <= now() and Lead_hours = '1'"
+    ) # Query written in InfluxQL
+
+
+gen_df = get_df(generation)
+wind_df = get_df(wind)
+
+####################################################
+## Preprocess the data
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+sns.pairplot(wind_df)
+plt.show()
+
+####################################################
+## Train model
+
