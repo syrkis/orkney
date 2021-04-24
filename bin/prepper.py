91fb2e1c992b35776db37142f920220fc83d20fc
Noah Syrkis <noah@syrkis.com>
2021-04-24 20:00:43 +0200

    time series augmentation for predict X

--- bin/prepper.py
+++ bin/prepper.py
@@ -19,13 +19,13 @@ class Prepper:
         weather = self.weather[self.weather['Source_time'] == self.weather['Source_time'].max()]
         self.weather = weather
 
-    def augment(self, n = 10):
+    def augment(self, n = 14):
         D = self.power.resample('3H').mean().merge(self.weather, how='outer', left_index=True, right_index=True)
-                   
+        X = self.weather.copy()
         for i in range(n):
             delta = np.array(D['Total'][-(self.weather.shape[0] - i + n) : -(n - i)])
-            ic(delta)
-        #D = self.weather.mergs(power, how='left', right_index=True, left_index=True)
+            X[f"t-{i}"] = delta
+        ic(X)
 
     def enhance(self):
         pass
