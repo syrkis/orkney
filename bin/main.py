6e4b2825201a2fca62bda20a356afdcf2912f409
Noah Syrkis <noah@syrkis.com>
2021-04-24 10:42:00 +0200

    fix: main

--- bin/main.py
+++ bin/main.py
@@ -28,39 +28,131 @@ wind  = client.query(
 gen_df = get_df(generation)
 wind_df = get_df(wind)
 
+
 ####################################################
-## Preprocessing class
+## Preprocessing classes
+
 from matplotlib import pyplot as plt
 import seaborn as sns
-import torch
-from torch import nn
 import math
-from pytorch_forecasting.data.timeseries import TimeSeriesDataSet
+import numpy as np
+from icecream import ic
 
-gen_df = gen_df[gen_df.columns[2]]
-wind_df = wind_df[wind_df.columns[[0, 3]]]
+def prepro(X, y=None):
+    print(**kw_args)
+    return y
 
+class Prepro:
 
-directions = ['E', 'ENE', 'NE', 'NNE', 'N', 'NNW', 'NW', 'WNW',
-          'W', 'WSW', 'SW', 'SSW', 'S', 'SSE', 'SE', 'ESE']
-radians = {d: (idx / 16) * 2 * math.pi for idx, d in enumerate(directions)}    
-long = lambda direction: round(math.cos(radians[direction]), 3)
-lat  = lambda direction: round(math.sin(radians[direction]), 3)
+    def __init__(self, piperun=True):
+        self.piperun = piperun
+ 
+    def fit(self, X, y=None): 
+        return self
 
-wind_df['long'] = wind_df[wind_df.columns[0]].apply(long)
-wind_df['lat'] = wind_df[wind_df.columns[0]].apply(lat)
+    def transform(self, X, y = None): 
+        if 't0' not in X.columns:
+            X = self.__augment(X, y)
+        X = self.__vectorize(X)
+        return X
 
-wind_df['long'] = wind_df['long'].multiply(wind_df['Speed'])
-wind_df['lat'] = wind_df['lat'].multiply(wind_df['Speed'])
+    def __augment(self, X, y = None, n = 10):
 
-wind_df = wind_df[wind_df.columns[2:]].resample('1Min', kind='timestamp').pad()
-data = gen_df.to_frame().join(wind_df)
-data = data.asfreq(pd.infer_freq(data.index))
-out = int(data.index.apply(lambda t: t * 10 ** 9))
-print(out)
-#M = TimeSeriesDataSet(data, 'time', 'Total', ['long', 'lat'])
+        if type(y) is type(None):
+            y = get_df(generation)['Total'] 
+        D = X.resample('1Min').pad() 
+        D = pd.merge(D, y, how='right', right_index=True, left_index=True)
+        D.rename(columns = {'Total': 't0'}, inplace=True)
+        X = D.copy().iloc[n:, :]
+        for i in range(1, n):
+            delta = D['t0'][n - i : - i] 
+            X[f"t-{i}"] = list(delta) 
+        return X.dropna()
 
+    def __vectorize(self, X):
+        directions = ['E', 'ENE', 'NE', 'NNE', 'N', 'NNW', 'NW', 'WNW',
+                  'W', 'WSW', 'SW', 'SSW', 'S', 'SSE', 'SE', 'ESE']
+        radians = {d: (idx / 16) * 2 * math.pi for idx, d in enumerate(directions)}    
+        long = lambda direction: round(math.cos(radians[direction]), 3)
+        lat  = lambda direction: round(math.sin(radians[direction]), 3)
+        
+        ic(X.columns)
+
+        X['long'] = X['Direction'].apply(long)
+        X['lat'] = X['Direction'].apply(lat)
+
+        X['long'] = X['long'].multiply(X['Speed'])
+        X['lat'] = X['lat'].multiply(X['Speed'])
+        
+        if self.piperun:
+            X = X[X.columns[X.columns != 'Direction']]
+        return X
+        
 
 ####################################################
-## Train model
-from torch.distributed.pipeline.sync import Pipe
+## Construct Pipeline and declare params
+
+from sklearn.pipeline import Pipeline
+import pickle
+from sklearn.svm import SVR
+
+pipeline = Pipeline([
+    ('prepro', Prepro()),
+    ('model', SVR())
+])
+
+# Fit the pipeline
+####################################################
+## Running
+
+#X, y = setup(wind_df, gen_df)
+
+# gs.fit(X_train, y_train)
+# print(gs.best_params_)
+
+X, y = wind_df, gen_df['Total'].dropna()
+prepro = Prepro(False)
+X = prepro.transform(X, y)
+y = X['t0']
+
+pipeline.fit(X, y)
+"""
+# Load stored model and compare with newly trained
+# and store the best one
+p_new = pipeline.predict(X_test)
+
+pipeline_old = pickle.load(open('../models/model.pkl', 'rb'))
+p_old = pipeline_old.predict(X_test)
+
+if mean_squared_error(p_new, y_test) < mean_squared_error(p_old, y_test):
+pickle.dump(pipeline, open('../models/model.pkl', 'wb'))
+print(mean_squared_error(p_old, y_test))
+print(mean_squared_error(p_new, y_test))
+print('NEW')
+else:
+pipeline = pipeline_old
+print(mean_squared_error(p_old, y_test))
+print(mean_squared_error(p_new, y_test))
+print('OLD')
+
+####################################################
+## Do forecasting with the best one
+
+# Get all future forecasts regardless of lead time
+forecasts = client.query(
+"SELECT * FROM MetForecasts where time > now()"
+) # Query written in InfluxQL
+for_wind_df = get_df(forecasts)
+
+for_gen_df = get_df
+print(sorted(list(map(int, list(pd.unique(for_df['Lead_hours']))))))
+# Limit to only the newest source time
+newest_source_time = for_df["Source_time"].max()
+newest_forecasts = for_df.loc[for_df["Source_time"] == newest_source_time].copy()
+
+# Preprocess the forecasts and do predictions in one fell swoop 
+# using your best pipeline.
+p = pipeline.predict(newest_forecasts)
+print(p)
+
+"""
